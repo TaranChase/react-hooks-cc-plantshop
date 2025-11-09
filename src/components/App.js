2da605944daac8285161361d3b0eaa09f85d4e93
TaranChase <chase.taran@gmail.com>
2025-11-09 21:46:21 +0300

    Added js using react to make the plantsy webpage interactive.

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
-import React from "react";
-import Header from "./Header";
-import PlantPage from "./PlantPage";
+import React, { useState, useEffect } from "react";
+import Search from "./Search";
+import NewPlantForm from "./NewPlantForm";
+import PlantList from "./PlantList";
 
 function App() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((r) => r.json())
+    .then((data) => setPlants(data));
+  }, []);
+
+  const displayedPlants = plants.filter((plant) => 
+  plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
+function handleAddPlant(newPlant) {
+  setPlants([...plants, newPlant]);
+}
+
   return (
     <div className="app">
-      <Header />
-      <PlantPage />
-    </div>
+      <h1>Plantsy Admin</h1>
+      <Search onSearchChange={setSearchTerm} />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <PlantList plants={displayedPlants} />
+      </div>
   );
 }
+
 
 export default App;
